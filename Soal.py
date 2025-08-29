3ed098eeb4e36dfffd34089ddfc6cbbbbe3dea37
Belvanz <belvansavero049@gmail.com>
2025-08-29 10:52:08 +0700

    Add files via upload
    Solusi soal 1 - 3

---
+++ Soal.py
@@ -0,0 +1,35 @@
+# # Soal 1
+# angka = int(input("Masukkan angka: "))
+
+# if angka % 2 == 0:
+#     print(angka, "adalah bilangan genap")
+# else:
+#     print(angka, "adalah bilangan ganjil")
+
+# # Soal 2
+# n = int(input("Masukkan angka N: "))
+# i = 1
+# total = 0
+
+# while i <= n:
+#     total+=i
+#     i+=1
+# print("Jumlah =", total)
+
+# # Soal 3
+# # Perulangan For
+# k = int(input("Masukkan angka: "))
+# print("Tabel perkalian", k,":")
+
+# for i in range(1, 11):
+#     print(k, "x", i, "=", k*i)
+
+# # Perulangan While
+# k = int(input("Masukkan angka: "))
+# i = 1
+# n = 10
+# print("Tabel perkalian", k,":")
+
+# while i <= n:
+#     print(k, "x", i, "=", k*i)
+#     i+=1
