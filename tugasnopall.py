7a12ff4f5e7bc4d633d79f78de6a12e37f03abe0
nopall339 <naufalbudi2009@gmail.com>
2025-08-29 11:30:52 +0700

    JUMAT 29 AGUSTUS

---
+++ tugasnopall.py
@@ -0,0 +1,20 @@
+# SOAL 1
+# angka = int(input("masukan angka anda: "))
+# if angka % 2 == 0:
+#     print(angka, "adalah bilangan genap")
+# else:
+#     print(angka, "adalah bilangan ganjil")
+
+# SOAL 2
+# angka = int(input("masukan nilai anda : "))
+# i= 0
+# jumlah = 0
+# while i <= angka:
+#     jumlah += i
+#     i += 1
+#     print(i," x ", no , "=", (i*no))
+
+# for
+# no = int(input("masukan nilai anda : "))
+# for i in range(0,10):
+#     print(no*(i+1))
